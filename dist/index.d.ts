ffd130c272f4631fbfbcdf250a885f0de3f068eb
Lucas Damian Johnson <60717791+lucasdamianjohnson@users.noreply.github.com>
2023-02-21 14:10:36 -0500

    updated

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -2,6 +2,6 @@ export * from "./DivineBinaryTags.js";
 export * from "./RemoteTagManager.js";
 export * from "./TagManager.js";
 export * from "./Classes/TagManagerBase.js";
-export * from "./Types/DBTSchema.types";
-export * from "./Types/Util.types";
+export * from "./Types/DBTSchema.types.js";
+export * from "./Types/Util.types.js";
 export * from "./Util/DBTUtil.js";
